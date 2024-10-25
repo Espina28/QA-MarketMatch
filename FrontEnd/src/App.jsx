67f47f53592be1494835985cac554667cd40fae6
BanDev <ivanjayadoptante@gmail.com>
2024-10-25 15:29:19 +0800

    App.jxs modified

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       <div>
         <Navbar />
         <Routes>
-          <Route path="/" element={<ProductDetail/>} /> {/* Homepage route */}
+          <Route path="/" element={<Welcome/>} /> {/* Homepage route */}
           <Route path="/sell-product" element={<UploadProduct />} /> {/* Upload Product route */}
           <Route path="/signup" element={<Signup />} /> {/* Order History route */}
           <Route path="/login" element={<Login />} /> {/* Order History route */}
